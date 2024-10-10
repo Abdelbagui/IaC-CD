fd39f36fdf0730f9c75200b23d39950ec66b2ddf
@Abdelbagui <a07abdel@gmail.com>
2024-10-10 14:28:15 +0200

    upgrade

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "location" {
 }
 
 
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
-variable "subscription_id" {}
+# variable "client_id" {}
+# variable "client_secret" {}
+# variable "tenant_id" {}
+# variable "subscription_id" {}
--- terraform/version.tf
+++ terraform/version.tf
@@ -20,9 +20,9 @@ terraform {
     container_name        = "abdelbagui"
     key                   = ".terraform.tfstate"
     
-    client_id             = var.client_id
-    client_secret         = var.client_secret
-    tenant_id             = var.tenant_id
-    subscription_id       = var.subscription_id
+    # client_id             = var.client_id
+    # client_secret         = var.client_secret
+    # tenant_id             = var.tenant_id
+    # subscription_id       = var.subscription_id
   }
 }

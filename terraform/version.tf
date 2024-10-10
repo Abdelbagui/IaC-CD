2602e9bddba42300fc9bdff061701c64a83b1c07
@Abdelbagui <a07abdel@gmail.com>
2024-10-10 14:41:34 +0200

    upgrade

--- terraform/version.tf
+++ terraform/version.tf
@@ -10,7 +10,8 @@ terraform {
 
 provider "azurerm" {
   features {}
-  
+  subscription_id       = "b8d59266-91c9-4141-a106-8ca2c5cd0155"
+ 
 }
 
 terraform {
@@ -19,10 +20,10 @@ terraform {
     storage_account_name  = "baguistorage"
     container_name        = "abdelbagui"
     key                   = ".terraform.tfstate"
-    
+   
+  } 
     # client_id             = var.client_id
     # client_secret         = var.client_secret
     # tenant_id             = var.tenant_id
     # subscription_id       = var.subscription_id
-  }
 }

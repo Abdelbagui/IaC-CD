eee4e5b448cc2c268065a062867be6729c72a542
@Abdelbagui <a07abdel@gmail.com>
2024-10-11 14:48:56 +0200

    upgrade

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,11 @@
 output "aks_cluster_api_url" {
-  value = azurerm_kubernetes_cluster.hasma_aks.id
+  value = azurerm_kubernetes_cluster.hasma_aks[0].id
+}
+
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.hasma_aks[0].name
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.hasma_rg[0].name
 }

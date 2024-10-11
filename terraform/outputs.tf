18bedde87db375a6f3b2bab15744e08e1fd6d08b
Hassan Abdelbagui <a07bagui@gmail.com>
2024-10-11 18:28:40 +0200

    test

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,10 @@
 output "aks_cluster_name" {
-  value = azurerm_kubernetes_cluster.hasma_aks[0].name
+  value = length(azurerm_kubernetes_cluster.hasma_aks) > 0 ? azurerm_kubernetes_cluster.hasma_aks[0].name : "Cluster non créé"
 }
 
 output "aks_cluster_api_url" {
-  value = azurerm_kubernetes_cluster.hasma_aks[0].fqdn
-  sensitive= true 
+  value = length(azurerm_kubernetes_cluster.hasma_aks) > 0 ? azurerm_kubernetes_cluster.hasma_aks[0].fqdn : "Cluster non créé"
+  sensitive = true 
 }
 
 output "resource_group_name" {

61abc6099fb7480a8377fee480b8fd3674c2cb67
Hassan Abdelbagui <a07bagui@gmail.com>
2024-10-11 18:05:50 +0200

    test

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -1,28 +1,23 @@
-# Récupérer le groupe de ressources s'il existe
-data "azurerm_resource_group" "existing_rg" {
-  name = var.resource_group_name
-}
-
- 
-data "azurerm_kubernetes_cluster" "existing_aks" {
-  name                = "abdel_HASMA_aks_cluster" # Nom du cluster AKS
-  resource_group_name = var.resource_group_name
-}
-
-# Créer le groupe de ressources seulement s'il n'existe pas
+# 1. Créer le groupe de ressources
 resource "azurerm_resource_group" "hasma_rg" {
-  count    = length(data.azurerm_resource_group.existing_rg) == 0 ? 1 : 0
   name     = var.resource_group_name
   location = var.location
 }
-# Définir le groupe de ressources s'il n'existe pas
 
-# Créer le cluster AKS
+# 2. Vérifier si le cluster AKS existe
+data "azurerm_kubernetes_cluster" "existing_aks" {
+  name                = var.aks_cluster_name
+  resource_group_name = var.resource_group_name
+  count               = 1  # Force Terraform à vérifier
+  depends_on          = [azurerm_resource_group.hasma_rg]
+}
+
+# 3. Créer le cluster AKS si nécessaire
 resource "azurerm_kubernetes_cluster" "hasma_aks" {
   count                = length(data.azurerm_kubernetes_cluster.existing_aks) == 0 ? 1 : 0
-  name                 = "abdel_HASMA_aks_cluster" # Remplacer par votre nom de cluster
-  location             = azurerm_resource_group.hasma_rg[0].location
-  resource_group_name  = azurerm_resource_group.hasma_rg[0].name
+  name                 = var.aks_cluster_name
+  location             = azurerm_resource_group.hasma_rg.location
+  resource_group_name  = azurerm_resource_group.hasma_rg.name
   dns_prefix           = "hasmak8s"
   
   default_node_pool {
@@ -41,13 +36,15 @@ resource "azurerm_kubernetes_cluster" "hasma_aks" {
     outbound_type     = "loadBalancer"
   }
 }
-# Obtenir les informations d'identification AKS pour utiliser kubectl
+
+# 4. Appliquer les manifestes Kubernetes après création du cluster
 resource "null_resource" "apply_k8s_manifests" {
   depends_on = [azurerm_kubernetes_cluster.hasma_aks]
 
-  provisioner "local-exec" {
+}
+provisioner "local-exec" {
     command = join("\n", [
-      "az aks get-credentials --resource-group ${azurerm_resource_group.hasma_rg[0].name} --name ${azurerm_kubernetes_cluster.hasma_aks[0].name}",
+      az aks get-credentials --resource-group ${azurerm_resource_group.hasma_rg.name} --name ${azurerm_kubernetes_cluster.hasma_aks[0].name} --overwrite-existing
       "kubectl apply -f ../Back",
       "kubectl apply -f ../Front",
       "kubectl apply -f ../Back/Phpmyadmin",
@@ -56,13 +53,4 @@ resource "null_resource" "apply_k8s_manifests" {
       "kubectl apply -f ../Monitoring/node-exporter --validate=false"
     ])
   }
-}
 
-# Outputs pour vérifier les valeurs créées
-output "resource_group_name" {
-  value = azurerm_resource_group.hasma_rg[0].name
-}
-
-output "aks_cluster_name" {
-  value = azurerm_kubernetes_cluster.hasma_aks[0].name
-}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,9 @@
-# output "aks_cluster_api_url" {
-#   value = length(data.azurerm_kubernetes_cluster.existing_aks) > 0 ? data.azurerm_kubernetes_cluster.existing_aks[0].kube_admin_config : azurerm_kubernetes_cluster.hasma_aks[0].kube_admin_config
-# sensitive = true
-# }
 
-# output "aks_cluster_name" {
-#   value = length(data.azurerm_kubernetes_cluster.existing_aks) > 0 ? data.azurerm_kubernetes_cluster.existing_aks[0].name : azurerm_kubernetes_cluster.hasma_aks[0].name
-# }
+# Outputs
+output "resource_group_name" {
+  value = azurerm_resource_group.hasma_rg.name
+}
+
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.hasma_aks[0].name
+}

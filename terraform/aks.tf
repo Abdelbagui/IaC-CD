576c725701fe1e7a36fe643652ac479cb10757fb
@Abdelbagui <a07abdel@gmail.com>
2024-10-11 16:57:29 +0200

    upgrade

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -1,16 +1,21 @@
+# Récupérer le groupe de ressources s'il existe
+data "azurerm_resource_group" "existing_rg" {
+  name = var.resource_group_name
+}
+
 # Créer le groupe de ressources seulement s'il n'existe pas
 resource "azurerm_resource_group" "hasma_rg" {
+  count    = length(data.azurerm_resource_group.existing_rg) == 0 ? 1 : 0
   name     = var.resource_group_name
   location = var.location
 }
 
-# Vérifier si le groupe de ressources existe avant de continuer
-
 # Créer le cluster AKS seulement s'il n'existe pas
 resource "azurerm_kubernetes_cluster" "hasma_aks" {
-  name                 = var.kubernetes_cluster_name
-  location             = var.location
-  resource_group_name  = var.resource_group_name.name
+  count                = length(data.azurerm_kubernetes_cluster.existing_aks) == 0 ? 1 : 0
+  name                 = "abdel_HASMA_aks_cluster" # Remplacer par votre nom de cluster
+  location             = azurerm_resource_group.hasma_rg[0].location
+  resource_group_name  = azurerm_resource_group.hasma_rg[0].name # Correction ici
   dns_prefix           = "hasmak8s"
 
   default_node_pool {
@@ -30,27 +35,13 @@ resource "azurerm_kubernetes_cluster" "hasma_aks" {
   }
 }
 
-# Vérification que le cluster AKS est opérationnel
-resource "null_resource" "wait_for_aks" {
-  provisioner "local-exec" {
-    command = <<EOT
-      echo "Attente que le cluster AKS soit prêt..."
-      while ! az aks show --resource-group ${azurerm_resource_group.hasma_rg.name} --name ${azurerm_kubernetes_cluster.hasma_aks.name} --query "powerState" -o tsv | grep -q "Running"; do
-        echo "Le cluster AKS n'est pas encore prêt. Attente de 10 secondes..."
-        sleep 10
-      done
-      echo "Le cluster AKS est prêt !"
-    EOT
-  }
-}
-
 # Obtenir les informations d'identification AKS pour utiliser kubectl
 resource "null_resource" "apply_k8s_manifests" {
-  depends_on = [null_resource.wait_for_aks]
+  depends_on = [azurerm_kubernetes_cluster.hasma_aks]
 
   provisioner "local-exec" {
     command = join("\n", [
-      "az aks get-credentials --resource-group ${azurerm_resource_group.hasma_rg.name} --name ${azurerm_kubernetes_cluster.hasma_aks.name}",
+      "az aks get-credentials --resource-group ${azurerm_resource_group.hasma_rg[0].name} --name ${azurerm_kubernetes_cluster.hasma_aks[0].name}",
       "kubectl apply -f ../Back",
       "kubectl apply -f ../Front",
       "kubectl apply -f ../Back/Phpmyadmin",
@@ -59,4 +50,13 @@ resource "null_resource" "apply_k8s_manifests" {
       "kubectl apply -f ../Monitoring/node-exporter --validate=false"
     ])
   }
+}
+
+# Outputs pour vérifier les valeurs créées
+output "resource_group_name" {
+  value = azurerm_resource_group.hasma_rg[0].name
+}
+
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.hasma_aks[0].name
 }

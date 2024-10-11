cfb638a60bdd446de3b9cfc88d7d29aacb96077d
@Abdelbagui <a07abdel@gmail.com>
2024-10-11 16:09:55 +0200

    upgrade

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -12,8 +12,9 @@ resource "azurerm_resource_group" "hasma_rg" {
 
 # Récupérer le cluster AKS s'il existe
 data "azurerm_kubernetes_cluster" "existing_aks" {
-  name                = var.kubernetes_cluster_name
-  resource_group_name = var.resource_group_name
+  count                = length(data.azurerm_resource_group.existing_rg) > 0 ? 1 : 0
+  name                 = var.kubernetes_cluster_name
+  resource_group_name  = var.resource_group_name
 }
 
 # Créer le cluster AKS seulement s'il n'existe pas
@@ -46,7 +47,7 @@ resource "null_resource" "wait_for_aks" {
   provisioner "local-exec" {
     command = <<EOT
       echo "Attente que le cluster AKS soit prêt..."
-      while ! az aks show --resource-group ${length(data.azurerm_resource_group.existing_rg) > 0 ? data.azurerm_resource_group.existing_rg.name : azurerm_resource_group.hasma_rg[0].name} --name ${length(data.azurerm_kubernetes_cluster.existing_aks) > 0 ? data.azurerm_kubernetes_cluster.existing_aks.name : azurerm_kubernetes_cluster.hasma_aks[0].name} --query "powerState" -o tsv | grep -q "Running"; do
+      while ! az aks show --resource-group ${length(data.azurerm_resource_group.existing_rg) > 0 ? data.azurerm_resource_group.existing_rg.name : azurerm_resource_group.hasma_rg[0].name} --name ${length(data.azurerm_kubernetes_cluster.existing_aks) > 0 ? data.azurerm_kubernetes_cluster.existing_aks[0].name : azurerm_kubernetes_cluster.hasma_aks[0].name} --query "powerState" -o tsv | grep -q "Running"; do
         echo "Le cluster AKS n'est pas encore prêt. Attente de 10 secondes..."
         sleep 10
       done
@@ -61,7 +62,7 @@ resource "null_resource" "apply_k8s_manifests" {
 
   provisioner "local-exec" {
     command = join("\n", [
-      "az aks get-credentials --resource-group ${length(data.azurerm_resource_group.existing_rg) > 0 ? data.azurerm_resource_group.existing_rg.name : azurerm_resource_group.hasma_rg[0].name} --name ${length(data.azurerm_kubernetes_cluster.existing_aks) > 0 ? data.azurerm_kubernetes_cluster.existing_aks.name : azurerm_kubernetes_cluster.hasma_aks[0].name}",
+      "az aks get-credentials --resource-group ${length(data.azurerm_resource_group.existing_rg) > 0 ? data.azurerm_resource_group.existing_rg.name : azurerm_resource_group.hasma_rg[0].name} --name ${length(data.azurerm_kubernetes_cluster.existing_aks) > 0 ? data.azurerm_kubernetes_cluster.existing_aks[0].name : azurerm_kubernetes_cluster.hasma_aks[0].name}",
       "kubectl apply -f ../Back",
       "kubectl apply -f ../Front",
       "kubectl apply -f ../Back/Phpmyadmin",

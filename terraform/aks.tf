9ff3aa4b04ede82567102560d81a8b702a3844de
@Abdelbagui <a07abdel@gmail.com>
2024-10-11 16:37:19 +0200

    upgrade

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -1,27 +1,20 @@
-# Vérification si le groupe de ressources existe
-data "azurerm_resource_group" "existing_rg" {
-  name = var.resource_group_name
-}
-
 # Créer le groupe de ressources seulement s'il n'existe pas
 resource "azurerm_resource_group" "hasma_rg" {
-  count    = length(data.azurerm_resource_group.existing_rg) == 0 ? 1 : 0
   name     = var.resource_group_name
   location = var.location
 }
 
-# Vérification si le cluster AKS existe
-data "azurerm_kubernetes_cluster" "existing_aks" {
-  name                = var.kubernetes_cluster_name
-  resource_group_name = length(data.azurerm_resource_group.existing_rg) > 0 ? data.azurerm_resource_group.existing_rg.name : azurerm_resource_group.hasma_rg[0].name
+# Vérifier si le groupe de ressources existe avant de continuer
+data "azurerm_resource_group" "existing_rg" {
+  name = var.resource_group_name
 }
 
 # Créer le cluster AKS seulement s'il n'existe pas
 resource "azurerm_kubernetes_cluster" "hasma_aks" {
   count                = length(data.azurerm_kubernetes_cluster.existing_aks) == 0 ? 1 : 0
   name                 = var.kubernetes_cluster_name
-  location             = length(data.azurerm_resource_group.existing_rg) > 0 ? data.azurerm_resource_group.existing_rg.location : azurerm_resource_group.hasma_rg[0].location
-  resource_group_name  = length(data.azurerm_resource_group.existing_rg) > 0 ? data.azurerm_resource_group.existing_rg.name : azurerm_resource_group.hasma_rg[0].name
+  location             = azurerm_resource_group.hasma_rg[0].location
+  resource_group_name  = azurerm_resource_group.hasma_rg[0].name
   dns_prefix           = "hasmak8s"
 
   default_node_pool {
@@ -46,7 +39,7 @@ resource "null_resource" "wait_for_aks" {
   provisioner "local-exec" {
     command = <<EOT
       echo "Attente que le cluster AKS soit prêt..."
-      while ! az aks show --resource-group ${length(data.azurerm_resource_group.existing_rg) > 0 ? data.azurerm_resource_group.existing_rg.name : azurerm_resource_group.hasma_rg[0].name} --name ${length(data.azurerm_kubernetes_cluster.existing_aks) > 0 ? data.azurerm_kubernetes_cluster.existing_aks.name : azurerm_kubernetes_cluster.hasma_aks[0].name} --query "powerState" -o tsv | grep -q "Running"; do
+      while ! az aks show --resource-group ${azurerm_resource_group.hasma_rg[0].name} --name ${azurerm_kubernetes_cluster.hasma_aks[0].name} --query "powerState" -o tsv | grep -q "Running"; do
         echo "Le cluster AKS n'est pas encore prêt. Attente de 10 secondes..."
         sleep 10
       done
@@ -61,7 +54,7 @@ resource "null_resource" "apply_k8s_manifests" {
 
   provisioner "local-exec" {
     command = join("\n", [
-      "az aks get-credentials --resource-group ${length(data.azurerm_resource_group.existing_rg) > 0 ? data.azurerm_resource_group.existing_rg.name : azurerm_resource_group.hasma_rg[0].name} --name ${length(data.azurerm_kubernetes_cluster.existing_aks) > 0 ? data.azurerm_kubernetes_cluster.existing_aks.name : azurerm_kubernetes_cluster.hasma_aks[0].name}",
+      "az aks get-credentials --resource-group ${azurerm_resource_group.hasma_rg[0].name} --name ${azurerm_kubernetes_cluster.hasma_aks[0].name}",
       "kubectl apply -f ../Back",
       "kubectl apply -f ../Front",
       "kubectl apply -f ../Back/Phpmyadmin",
@@ -70,4 +63,17 @@ resource "null_resource" "apply_k8s_manifests" {
       "kubectl apply -f ../Monitoring/node-exporter --validate=false"
     ])
   }
+}
+
+
+
+
+
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "-Win1-Serv-2022"  # Utilisez la variable ici
+    storage_account_name  = "baguistorage"
+    container_name        = "abdelbagui"
+    key                   = ".terraform.tfstate"
+  } 
 }

f702048e49c191c3d2e484266b21ad2ebde088f4
@Abdelbagui <a07abdel@gmail.com>
2024-10-11 16:19:54 +0200

    upgrade

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,8 @@
-output "aks_cluster_api_url" {
-  value = length(data.azurerm_kubernetes_cluster.existing_aks) > 0 ? data.azurerm_kubernetes_cluster.existing_aks[0].kube_admin_config : azurerm_kubernetes_cluster.hasma_aks[0].kube_admin_config
-  sensitive = true
-}
+# output "aks_cluster_api_url" {
+#   value = length(data.azurerm_kubernetes_cluster.existing_aks) > 0 ? data.azurerm_kubernetes_cluster.existing_aks[0].kube_admin_config : azurerm_kubernetes_cluster.hasma_aks[0].kube_admin_config
+# sensitive = true
+# }
 
-output "aks_cluster_name" {
-  value = length(data.azurerm_kubernetes_cluster.existing_aks) > 0 ? data.azurerm_kubernetes_cluster.existing_aks[0].name : azurerm_kubernetes_cluster.hasma_aks[0].name
-}
+# output "aks_cluster_name" {
+#   value = length(data.azurerm_kubernetes_cluster.existing_aks) > 0 ? data.azurerm_kubernetes_cluster.existing_aks[0].name : azurerm_kubernetes_cluster.hasma_aks[0].name
+# }

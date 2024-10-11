a78d1c474dd5a0e8d2a1979a76cef56ca28f3cda
@Abdelbagui <a07abdel@gmail.com>
2024-10-11 15:35:12 +0200

    upgrade

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,14 @@
 output "aks_cluster_api_url" {
-  value = length(data.azurerm_kubernetes_cluster.existing_aks) > 0 ? data.azurerm_kubernetes_cluster.existing_aks.kube_admin_config : azurerm_kubernetes_cluster.hasma_aks[0].kube_admin_config
+  value     = length(data.azurerm_kubernetes_cluster.existing_aks) > 0 ? data.azurerm_kubernetes_cluster.existing_aks.kube_admin_config : azurerm_kubernetes_cluster.hasma_aks[0].kube_admin_config
+  sensitive = true  # Marquer comme sensible
 }
 
 output "aks_cluster_name" {
-  value = length(data.azurerm_kubernetes_cluster.existing_aks) > 0 ? data.azurerm_kubernetes_cluster.existing_aks.name : azurerm_kubernetes_cluster.hasma_aks[0].name
+  value     = length(data.azurerm_kubernetes_cluster.existing_aks) > 0 ? data.azurerm_kubernetes_cluster.existing_aks.name : azurerm_kubernetes_cluster.hasma_aks[0].name
+  sensitive = false # Pas sensible
 }
 
 output "resource_group_name" {
-  value = length(data.azurerm_resource_group.existing_rg) > 0 ? data.azurerm_resource_group.existing_rg.name : azurerm_resource_group.hasma_rg[0].name
+  value     = length(data.azurerm_resource_group.existing_rg) > 0 ? data.azurerm_resource_group.existing_rg.name : azurerm_resource_group.hasma_rg[0].name
+  sensitive = false # Pas sensible
 }

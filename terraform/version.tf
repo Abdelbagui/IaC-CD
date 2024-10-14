d2c06b9595b2a439e7297d24018070985c7d9230
@Abdelbagui <a07abdel@gmail.com>
2024-10-14 14:56:48 +0200

    upgrade

--- terraform/version.tf
+++ terraform/version.tf
@@ -2,15 +2,18 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 3.0"  # ou la dernière version stable
     }
   }
+
+  required_version = ">= 1.0.0"
+
   backend "azurerm" {
-    resource_group_name   = "-Win1-Serv-2022"  # Utilisez la variable ici
-    storage_account_name  = "baguistorage"
-    container_name        = "abdelbagui"
-    key                   = ".terraform.tfstate"
-  } 
+    resource_group_name  = "-Win1-Serv-2022"  # Utilisez la variable ici
+    storage_account_name = "baguistorage"
+    container_name       = "abdelbagui"
+    key                  = ".terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
